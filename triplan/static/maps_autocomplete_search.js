cd45abf383a599547fcccef623622b2789e2c0f6
Linna Wang <linna.wang15@imperial.ac.uk>
2017-07-17 21:12:51 +0100

    set default map center to singapore

--- triplan/static/maps_autocomplete_search.js
+++ triplan/static/maps_autocomplete_search.js
@@ -5,7 +5,7 @@ function initAutocomplete() {
     // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        // center: {lat: -33.8688, lng: 151.2195},
+        center: {lat: 1.3521, lng: 103.8198},
         zoom: 13,
         mapTypeId: 'roadmap'
     });
